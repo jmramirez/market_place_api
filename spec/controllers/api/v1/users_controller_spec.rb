eef7553876a24f80109763dbacc306adf08bbbea
Jose <joshem31@gmail.com>
2018-02-13 16:00:45 -0500

    Refactors test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::V1:: like the path spec/controllers/api/v1/
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1, application/json" } # run before each test in this scope, Mime::JSON lets us remove every `format: :json` param to requests
 
   describe "GET #show" do
     before(:each) do # run before each test in this scope
       @user = FactoryBot.create :user # create (so save also?) the instance of User model
-      get :show, params: { id: @user.id , format: :json } # issue GET request to UsersController#show, with id param of @user.id and in format of json
+      get :show, params: { id: @user.id  } # issue GET request to UsersController#show, with id param of @user.id and in format of json
     end
 
     it "returns the information about a reporter on a hash" do
@@ -22,7 +21,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::
     context "when is successfully created" do
       before(:each) do
         @user_attributes = FactoryBot.attributes_for :user
-        post :create, params: { user: @user_attributes }, format: :json
+        post :create, params: { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
@@ -38,7 +37,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::
       before(:each) do
         #notice I'm not including the email
         @invalid_user_attributes = { password: "12345678",password_confirmation: "12345678"}
-        post :create, params: { user: @invalid_user_attributes }, format: :json
+        post :create, params: { user: @invalid_user_attributes }
       end
 
       it "renders an errors json" do
@@ -61,7 +60,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::
     context "when is successfully updated" do
       before(:each) do
         @user = FactoryBot.create :user
-        patch :update, params: {id: @user.id, user: {email: "newmail@example.com"}}, format: :json
+        patch :update, params: {id: @user.id, user: {email: "newmail@example.com"}}
       end
 
       it "renders the json representation for the update user" do
@@ -75,7 +74,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::
     context "when is not created" do
       before(:each) do
         @user = FactoryBot.create :user
-        patch :update, params: {id: @user.id, user: {email: "badmail.com"}}, format: :json
+        patch :update, params: {id: @user.id, user: {email: "badmail.com"}}
       end
 
       it "renders an errors json" do
@@ -96,7 +95,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryBot.create :user
-      delete :destroy, params: { id: @user.id }, format: :json
+      delete :destroy, params: { id: @user.id }
     end
 
     it { should respond_with 204 }

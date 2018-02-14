e337d9d75d0106881e977925aada7dc864f18485
Jose <joshem31@gmail.com>
2018-02-14 12:28:00 -0500

    Adds product model bare bones along with some validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214040336) do
+ActiveRecord::Schema.define(version: 20180214162430) do
+
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title", default: ""
+    t.decimal "price", precision: 10, default: "0"
+    t.boolean "published", default: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false

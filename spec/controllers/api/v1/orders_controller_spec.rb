480fadb2dc05933fdabb597aa1ddb0fa169c2408
Jose <joshem31@gmail.com>
2018-02-19 14:30:24 -0500

    Refactors the pagination behavior for index actions

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -15,11 +15,7 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
       expect(orders_response.count).to eq(4)
     end
 
-    it { expect(json_response).to have_key(:meta)}
-    it { expect(json_response[:meta]).to have_key(:pagination)}
-    it { expect(json_response[:meta][:pagination]).to have_key(:per_page)}
-    it { expect(json_response[:meta][:pagination]).to have_key(:total_pages)}
-    it { expect(json_response[:meta][:pagination]).to have_key(:total_objects)}
+    it_behaves_like "paginated list"
 
     it {should respond_with 200 }
 
